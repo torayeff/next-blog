baa81909398529f83b6a20eb78a6299c66b037be
torayeff <torayeff@gmail.com>
2020-06-07 10:04:01 +0200

    Toggle

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [isBurgerActive, activateBurger] = useState(false);
+
+  const onClick = () => {
+    console.log("clicked");
+    activateBurger(!isBurgerActive);
+  }
+
   return (
     <div className="columns is-centered">
       <div className="column is-8">
@@ -11,20 +18,23 @@ export default function Header() {
               <a className="navbar-item" href="../">
                 <img src="/logo.png" alt="Logo"/>
               </a>
-              <span className="navbar-burger burger" data-target="navbarMenu">
-              <span/>
-              <span/>
-              <span/>
-            </span>
+              <span className={`navbar-burger burger ${isBurgerActive ? "is-active" : ""}`}
+                    data-target="navbarMenu"
+              onClick={onClick}>
+                <span/>
+                <span/>
+                <span/>
+              </span>
             </div>
 
-            <div id="navbarMenu" className="navbar-menu">
+            <div id="navbarMenu" className={`navbar-menu ${isBurgerActive ? "is-active" : ""}`}>
               <div className="navbar-end">
                 <a className="navbar-item">About me</a>
                 <a className="navbar-item">Blog</a>
                 <a className="navbar-item">Projects</a>
               </div>
             </div>
+
           </div>
         </nav>
       </div>
